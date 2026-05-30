de82dab676b657c2adb181c4d0664e2d92c9eab8
vYtHarIk <giopagolif25@gmail.com>
2026-05-30 23:45:25 +0300

    Fix build paths for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import obfuscator from 'rollup-plugin-obfuscator';
 
 export default defineConfig({
-  base: './',
+  base: '/watchwiki/',
   plugins: [
     react(),
     obfuscator({
